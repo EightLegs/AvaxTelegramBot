92c669f16f93970d13a1422671a6066825dcbde0
EightLegs <esmitin17@mail.ru>
2022-04-01 22:39:27 +0300

    Working on Functions

--- AvaxTelegramBot/Model/Handle.cs
+++ AvaxTelegramBot/Model/Handle.cs
@@ -44,7 +44,15 @@ namespace AvaxTelegramBot.Model
 
         private static async Task HandleEveryTenSecondsMessage(ITelegramBotClient botClient, Update update)
         {
-            await HandleTimer(botClient, update);
+            try 
+            {
+                await HandleTimer(botClient, update);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+                await botClient.SendTextMessageAsync(update.Message.Chat, ex.Message);
+            }
             /*if (Timer1 != null && Timer1.Enabled)
             {
                 Timer1.Stop();
@@ -60,23 +68,63 @@ namespace AvaxTelegramBot.Model
         }
         private static async Task HandleTimer(ITelegramBotClient botClient, Update update)
         {
+            //TO DO проверить переполнение ID блока
+            if (((Bot)botClient).LastBlockID == null)
+            {
+                ((Bot)botClient).LastBlockID = GetLastBlockID(botClient);
+                await botClient.SendTextMessageAsync(update.Message.Chat, "Установлен блок с ID " + ((Bot)botClient).LastBlockID);
+                return; 
+            }
+
+            string oldLastBlockID = ((Bot)botClient).LastBlockID;
+            ((Bot)botClient).LastBlockID = GetLastBlockID(botClient);
+
+            
+            List<Block> newBlocks = new List<Block>();
+            ulong difference = ulong.Parse(((Bot)botClient).LastBlockID) - ulong.Parse(oldLastBlockID);
+
+            //for(ulong i = ulong.Parse(oldLastBlockID) + 1; i < ulong.Parse(((Bot)botClient).LastBlockID); ++i)
+
+            List<Block> blocks = GetBlocks(botClient, ulong.Parse(oldLastBlockID), ulong.Parse(((Bot)botClient).LastBlockID));
+
+            //await botClient.SendTextMessageAsync(update.Message.Chat, "Новые  " + oldLastBlockID + " " + ((Bot)botClient).LastBlockID);
+            //await botClient.SendTextMessageAsync(update.Message.Chat, "Разница " + difference);
+            
+
+
+        }
+        //TO DO возможно перенести функции в класс Bot
+        private static string GetLastBlockID(ITelegramBotClient botClient)
+        {
+            string? lastIdBlock = null;
             using (WebClient wc = new WebClient())
             {
-
-                if (((Bot)botClient).LastBlockID == null)
+                string jsonString = wc.DownloadString(String.Format("https://api.snowtrace.io/api?module=block&action=getblocknobytime&timestamp={0}&closest=before&apikey={1}", DateTimeOffset.Now.ToUnixTimeSeconds().ToString(), ((Bot)botClient).ApiKey));
+                JObject json = JObject.Parse(jsonString);
+                if (json["status"].ToString() == "1")
                 {
-                    string jsonString = wc.DownloadString(String.Format("https://api.snowtrace.io/api?module=block&action=getblocknobytime&timestamp={0}&closest=before&apikey={1}", DateTimeOffset.Now.ToUnixTimeSeconds().ToString(), ((Bot)botClient).ApiKey));
-                    JObject json = JObject.Parse(jsonString);
-                    if(json["status"].ToString() == "1")
-                        ((Bot)botClient).LastBlockID = json["result"].ToString();
-                    int b = 5;
-                }
-                else
-                {
-
+                    lastIdBlock = json["result"].ToString();
                 }
             }
-            await botClient.SendTextMessageAsync(update.Message.Chat, "Привет-привет!!");
+            if (lastIdBlock != null)
+                return lastIdBlock;
+            else throw new Exception("Couldn`t get last block ID (GetLastBlockID)");
+        }
+        private static List<Block> GetBlocks(ITelegramBotClient botClient, ulong firstBlock, ulong lastBlock)
+        {
+            List<Block> blocks = new List<Block>();
+            using (WebClient wc = new WebClient())
+            {
+                for (ulong i = firstBlock; i < lastBlock; ++i)
+                {
+                    string jsonString = wc.DownloadString(String.Format("https://api.snowtrace.io/api?module=block&action=getblockreward&blockno={0}&apikey={1}", i.ToString(), ((Bot)botClient).ApiKey));
+                    JObject json = JObject.Parse(jsonString);
+                    //blocks.Add(new Block())
+                }
+            }
+
+
+            return blocks;
         }
     }
 }
